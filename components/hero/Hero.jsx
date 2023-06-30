9622db47670a74f56640df34b25b2e47d4b77d2f
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2023-06-30 22:49:27 +0530

    feat. Add Basic Hero Content

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
-import { HeroSectionContainer } from './Hero.styled';
 import Typography from '../display/typography/Typography';
 
+import dynamic from 'next/dynamic';
+const Animator = dynamic(
+  import('react-scroll-motion').then((it) => it.Animator),
+  { ssr: false }
+);
+import {
+  ScrollContainer,
+  ScrollPage,
+  batch,
+  Fade,
+  FadeIn,
+  FadeOut,
+  Move,
+  MoveIn,
+  MoveOut,
+  Sticky,
+  StickyIn,
+  StickyOut,
+  Zoom,
+  ZoomIn,
+  ZoomOut,
+} from 'react-scroll-motion';
+import {
+  HeroSectionContainer,
+  HeroTextContainer,
+  HeroTextSpan,
+  LogoContainer,
+} from './Hero.styled.js';
+import GDSCLogo from '@logos/gdsc-logo.svg';
 const Hero = () => {
   return (
     <HeroSectionContainer>
-      <Typography variant='h1'>Learn.Connect.Grow</Typography>
+      <ScrollContainer>
+        <ScrollPage>
+          <Animator animation={batch(Fade(), Sticky(), Zoom(8, 1))}>
+            <LogoContainer>
+              <GDSCLogo className='gdscLogo' />
+            </LogoContainer>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage>
+          <Animator animation={batch(Fade(), Sticky(), MoveIn(0, -200))}>
+            <HeroTextSpan color='#EA4335'>Learn.</HeroTextSpan>
+            <HeroTextSpan color='#4285F4'>Connect.</HeroTextSpan>
+            <HeroTextSpan color='#0F9D58'>Grow.</HeroTextSpan>
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer>
     </HeroSectionContainer>
   );
 };
--- components/hero/Hero.styled.js
+++ components/hero/Hero.styled.js
@@ -2,9 +2,41 @@ import { styled } from 'styled-components';
 
 export const HeroSectionContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.bgPrimary};
-  height: 100vh;
+  height: 200vh;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  height: 400px;
+  overflow: hidden;
+`;
+
+export const HeroTextContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HeroTextSpan = styled.span`
+  font-size: 70px;
+  font-weight: 700;
+  width: 100%;
+  color: ${({ theme }) => theme.colors.contentPrimary};
+  margin: 0 10px;
+  transition: font-size 0.5s ease-in-out, color 0.2s ease-in-out;
+  cursor: pointer;
+  &:hover {
+    font-size: 86px;
+    color: ${({ color }) => (color ? color : '#FFF06C')};
+  }
 `;
