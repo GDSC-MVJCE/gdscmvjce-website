79d14508d82a2a07d548d64cfd17f20ee078368e
Shivam77607 <shivamsharma77607@gmail.com>
2023-07-01 14:13:31 +0530

    refact: add colors

--- constants/theme.js
+++ constants/theme.js
@@ -1,83 +1,85 @@
 export const lightTheme = {
-  type: 'light',
-  colors: {
-    brandPrimary: '#FFF06C',
-    bgPrimary: '#FFFFFF',
-    bgSecondary: '#E9E9E9',
-    bgTertiary: '#989898',
-    contentPrimary: '#131313',
-    utilRed: '#FF2727',
-    utilGreen: '#30B700',
-  },
+	type: "light",
+	colors: {
+		brandRed: "#EA4335",
+		brandYellow: "#FBBC04",
+		brandGreen: "#0F9D58",
+		brandBlue: "#4285F4",
+		bgPrimary: "#FFFFFF",
+		bgSecondary: "#E9E9E9",
+		bgTertiary: "#989898",
+		contentPrimary: "#131313"
+	}
 };
 
 export const darkTheme = {
-  type: 'dark',
-  colors: {
-    brandPrimary: '#FFF06C',
-    bgPrimary: '#131313',
-    bgSecondary: '#292929',
-    bgTertiary: '#989898',
-    contentPrimary: '#FFFFFF',
-    contentSecondary: '#858585',
-    utilRed: '#FF2727',
-    utilGreen: '#30B700',
-  },
+	type: "dark",
+	colors: {
+		brandRed: "#EA4335",
+		brandYellow: "#FBBC04",
+		brandGreen: "#0F9D58",
+		brandBlue: "#4285F4",
+		bgPrimary: "#131313",
+		bgSecondary: "#292929",
+		bgTertiary: "#989898",
+		contentPrimary: "#FFFFFF",
+		contentSecondary: "#858585"
+	}
 };
 
 export const typography = {
-  h1: {
-    fontWeight: '700',
-    fontSize: '40px',
-    lineHeight: '55px',
-  },
-  h2: {
-    fontWeight: '600',
-    fontSize: '32px',
-    lineHeight: '44px',
-  },
-  h3: {
-    fontWeight: '600',
-    fontSize: '28px',
-    lineHeight: '38px',
-  },
-  h4: {
-    fontWeight: '600',
-    fontSize: '24px',
-    lineHeight: '33px',
-  },
-  h5: {
-    fontWeight: '600',
-    fontSize: '20px',
-    lineHeight: '27px',
-  },
-  h6: {
-    fontWeight: '600',
-    fontSize: '16px',
-    lineHeight: '22px',
-  },
-  body: {
-    fontWeight: '400',
-    fontSize: '18px',
-    lineHeight: '25px',
-  },
-  bodyEmphasized: {
-    fontWeight: '500',
-    fontSize: '18px',
-    lineHeight: '25px',
-  },
-  bodySmall: {
-    fontWeight: '400',
-    fontSize: '14px',
-    lineHeight: '19px',
-  },
-  bodySmallEmphasized: {
-    fontSize: '16px',
-    fontWeight: '600',
-  },
-  caption: {
-    fontWeight: '400',
-    fontSize: '10px',
-    lineHeight: '14px',
-  },
+	h1: {
+		fontWeight: "700",
+		fontSize: "40px",
+		lineHeight: "55px"
+	},
+	h2: {
+		fontWeight: "600",
+		fontSize: "32px",
+		lineHeight: "44px"
+	},
+	h3: {
+		fontWeight: "600",
+		fontSize: "28px",
+		lineHeight: "38px"
+	},
+	h4: {
+		fontWeight: "600",
+		fontSize: "24px",
+		lineHeight: "33px"
+	},
+	h5: {
+		fontWeight: "600",
+		fontSize: "20px",
+		lineHeight: "27px"
+	},
+	h6: {
+		fontWeight: "600",
+		fontSize: "16px",
+		lineHeight: "22px"
+	},
+	body: {
+		fontWeight: "400",
+		fontSize: "18px",
+		lineHeight: "25px"
+	},
+	bodyEmphasized: {
+		fontWeight: "500",
+		fontSize: "18px",
+		lineHeight: "25px"
+	},
+	bodySmall: {
+		fontWeight: "400",
+		fontSize: "14px",
+		lineHeight: "19px"
+	},
+	bodySmallEmphasized: {
+		fontSize: "16px",
+		fontWeight: "600"
+	},
+	caption: {
+		fontWeight: "400",
+		fontSize: "10px",
+		lineHeight: "14px"
+	}
 };
