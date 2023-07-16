d81bf365a4fc5b7198f6ec7cf901a47d27f39b35
Shivam77607 <shivamsharma77607@gmail.com>
2023-07-16 12:17:39 +0530

    refact: remove navbar

--- pages/_app.js
+++ pages/_app.js
@@ -4,31 +4,28 @@ import { lightTheme } from "@/constants/theme";
 
 import localFont from "next/font/local";
 
-import Navbar from "@/components/navbar/Navbar";
-
 const myFont = localFont({
-  src: [
-    {
-      path: "../public/fonts/GoogleSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/GoogleSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-  ],
+	src: [
+		{
+			path: "../public/fonts/GoogleSans-Regular.ttf",
+			weight: "400",
+			style: "normal"
+		},
+		{
+			path: "../public/fonts/GoogleSans-Bold.ttf",
+			weight: "700",
+			style: "normal"
+		}
+	]
 });
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyles />
-      <main className={myFont.className}>
-        <Navbar />
-        <Component {...pageProps} />
-      </main>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={lightTheme}>
+			<GlobalStyles />
+			<main className={myFont.className}>
+				<Component {...pageProps} />
+			</main>
+		</ThemeProvider>
+	);
 }
