bc685a7cd3a0c96ee23c9841f2da73ba5127d708
karanadam <karanadamv@gmail.com>
2023-07-28 00:36:55 +0530

    refactor:  fix gap

--- components/meetTeam/MeetTeam.styled.js
+++ components/meetTeam/MeetTeam.styled.js
@@ -17,20 +17,22 @@ export const LeftContainer = styled.div`
 `;
 
 export const RightContainer = styled.div`
-  flex: 40%;
+  flex: 50%;
   display: flex;
   flex-direction: column;
   padding: 2em 2em;
   justify-content: center;
   align-items: start;
+  gap: 1em;
 `;
 
 export const MeetButton = styled.button`
-  height: 50px;
-  width: 180px;
+  height: 54px;
+  width: 196px;
   border-radius: 16px;
   font-size: 20px;
   background-color: #d4e3ff;
   border: 2px solid #4285f4;
+  font-weight: 600;
   cursor: pointer;
 `;
