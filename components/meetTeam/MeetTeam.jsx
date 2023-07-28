667d89cff8b1991704c4e484d7a4e949ad244162
karanadam <karanadamv@gmail.com>
2023-07-28 18:38:22 +0530

    refactor: remove classname from main container

--- components/meetTeam/MeetTeam.jsx
+++ components/meetTeam/MeetTeam.jsx
@@ -5,7 +5,7 @@ import Typography from '../display/typography/Typography'
 
 function MeetTeam() {
     return <MainContainer>
-        <LeftContainer className="container">
+        <LeftContainer>
             <Avatar size='xl' borderColor={'#4285F4'} borderWidth={'0.7em'} url={'https://avatars.githubusercontent.com/u/35398866?v=4'} className="first" />
             <Avatar size='xl' borderColor={'#0F9D58'} borderWidth={'0.7em'} url={'https://avatars.githubusercontent.com/u/94698713?v=4'} className="second" />
             <Avatar size='xl' borderColor={'#EA4335'} borderWidth={'0.7em'} url={'https://avatars.githubusercontent.com/u/102166167?v=4'}
