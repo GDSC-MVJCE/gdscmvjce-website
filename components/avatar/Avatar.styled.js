6ebe7dd654446691519682c71202c71ba24490ad
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-03 21:53:32 +0530

    feat: add lg size

--- components/avatar/Avatar.styled.js
+++ components/avatar/Avatar.styled.js
@@ -1,49 +1,54 @@
 import { css, styled } from "styled-components";
 
 export const AvatarWrapper = styled.div`
-  ${({ size }) =>
-    (size === "xs" &&
-      css`
-        width: 32px;
-        height: 32px;
-      `) ||
-    (size === "sm" &&
-      css`
-        width: 47px;
-        height: 47px;
-      `) ||
-    (size === "md" &&
-      css`
-        width: 75px;
-        height: 75px;
-      `) ||
-    (size === "xl" &&
-      css`
-        width: 232px;
-        height: 232px;
-      `)};
-  border-width: ${({ borderWidth }) => (borderWidth ? borderWidth : "3px")};
-  border-color: ${({ borderColor, theme }) =>
-    borderColor ? borderColor : theme.colors.bgPrimary};
-  background: url(${({ url }) => url}) no-repeat center center;
-  background-size: cover;
-  border-style: solid;
-  border-radius: 50%;
-  cursor: pointer;
+	${({ size }) =>
+		(size === "xs" &&
+			css`
+				width: 32px;
+				height: 32px;
+			`) ||
+		(size === "sm" &&
+			css`
+				width: 47px;
+				height: 47px;
+			`) ||
+		(size === "md" &&
+			css`
+				width: 75px;
+				height: 75px;
+			`) ||
+		(size === "lg" &&
+			css`
+				width: 100px;
+				height: 100px;
+			`) ||
+		(size === "xl" &&
+			css`
+				width: 232px;
+				height: 232px;
+			`)};
+	border-width: ${({ borderWidth }) => (borderWidth ? borderWidth : "3px")};
+	border-color: ${({ borderColor, theme }) =>
+		borderColor ? borderColor : theme.colors.bgPrimary};
+	background: url(${({ url }) => url}) no-repeat center center;
+	background-size: cover;
+	border-style: solid;
+	border-radius: 50%;
+	cursor: pointer;
 
-  &.first {
-    z-index: 1;
-    position: absolute;
-    left: 0px;
-  }
-  &.second {
-    z-index: 3;
-    position: absolute;
-    left: 150px;
-  }
-  &.third {
-    z-index: 2;
-    position: absolute;
-    left: 300px;
-  }
+	&.first {
+		z-index: 1;
+		position: absolute;
+		left: 0px;
+	}
+	&.second {
+		z-index: 3;
+		position: absolute;
+		left: 150px;
+	}
+	&.third {
+		z-index: 2;
+		position: absolute;
+		left: 300px;
+	}
 `;
