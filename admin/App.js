4af027abcbd31d63655a009fe91792506bfecbb1
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2023-08-11 17:27:04 +0530

    Add Labels & Refactor Prisma Schema

--- admin/App.js
+++ admin/App.js
@@ -4,65 +4,71 @@ import {
   People,
   PeopleAlt,
   PeopleAltOutlined,
-} from "@mui/icons-material";
-import { dataProvider } from "ra-data-simple-prisma";
-import React from "react";
-import { Admin, Resource } from "react-admin";
+} from '@mui/icons-material';
+import { dataProvider } from 'ra-data-simple-prisma';
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
 
-import BlogTagCreate from "./BlogTag/BlogTagCreate";
-import BlogTagEdit from "./BlogTag/BlogTagEdit";
-import BlogTagList from "./BlogTag/BlogTagList";
-import EventCreate from "./Event/EventCreate";
-import EventEdit from "./Event/EventEdit";
-import EventList from "./Event/EventList";
-import EventTagCreate from "./EventTag/EventTagCreate";
-import EventTagEdit from "./EventTag/EventTagEdit";
-import EventTagList from "./EventTag/EventTagList";
-import { MenuLayout } from "./MenuLayout";
-import SpeakerCreate from "./Speaker/SpeakerCreate";
-import SpeakerEdit from "./Speaker/SpeakerEdit";
-import SpeakerList from "./Speaker/SpeakerList";
-import TeamCreate from "./Teams/TeamCreate";
-import TeamEdit from "./Teams/TeamEdit";
-import TeamList from "./Teams/TeamList";
-import UserEdit from "./Users/UserEdit";
-import UserList from "./Users/UserList";
+import BlogTagCreate from './BlogTag/BlogTagCreate';
+import BlogTagEdit from './BlogTag/BlogTagEdit';
+import BlogTagList from './BlogTag/BlogTagList';
+import EventCreate from './Event/EventCreate';
+import EventEdit from './Event/EventEdit';
+import EventList from './Event/EventList';
+import EventTagCreate from './EventTag/EventTagCreate';
+import EventTagEdit from './EventTag/EventTagEdit';
+import EventTagList from './EventTag/EventTagList';
+import { MenuLayout } from './MenuLayout';
+import SpeakerCreate from './Speaker/SpeakerCreate';
+import SpeakerEdit from './Speaker/SpeakerEdit';
+import SpeakerList from './Speaker/SpeakerList';
+import TeamCreate from './Teams/TeamCreate';
+import TeamEdit from './Teams/TeamEdit';
+import TeamList from './Teams/TeamList';
+import UserEdit from './Users/UserEdit';
+import UserList from './Users/UserList';
 
 const App = () => {
   return (
-    <Admin layout={MenuLayout} dataProvider={dataProvider("/api/admin")}>
+    <Admin layout={MenuLayout} dataProvider={dataProvider('/api/admin')}>
       <Resource
-        name="event"
+        name='event'
         icon={Event}
         list={EventList}
         create={EventCreate}
         edit={EventEdit}
       />
-      <Resource name="user" icon={People} list={UserList} edit={UserEdit} />
+      <Resource name='user' icon={People} list={UserList} edit={UserEdit} />
       <Resource
-        name="speaker"
+        name='speaker'
         icon={PeopleAltOutlined}
         list={SpeakerList}
         create={SpeakerCreate}
         edit={SpeakerEdit}
       />
       <Resource
-        name="team"
+        name='team'
         icon={PeopleAlt}
         list={TeamList}
         create={TeamCreate}
         edit={TeamEdit}
       />
       <Resource
-        name="eventTag"
+        name='eventTag'
         icon={Label}
+        options={{
+          label: 'Event Tags',
+        }}
         list={EventTagList}
         create={EventTagCreate}
         edit={EventTagEdit}
       />
       <Resource
-        name="blogTag"
+        name='blogTag'
         icon={Label}
+        options={{
+          label: 'Blog Tags',
+        }}
         list={BlogTagList}
         create={BlogTagCreate}
         edit={BlogTagEdit}
