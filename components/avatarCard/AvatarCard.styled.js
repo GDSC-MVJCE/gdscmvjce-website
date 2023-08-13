76f54431f0a9a87dcac66b07484ae44c5932915f
karanadam <karanadamv@gmail.com>
2023-08-13 18:13:38 +0530

    feat: add team page

---
+++ components/avatarCard/AvatarCard.styled.js
@@ -0,0 +1,20 @@
+import { styled } from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: auto;
+  height: auto;
+  gap: 1em;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
---
+++ pages/teams/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '@/components/navbar/Navbar'
+import TeamsPage from '@/components/teamsPage/teamsPage'
+
+function teams() {
+    return (
+        <>
+            <Navbar />
+            <TeamsPage />
+        </>
+    )
+
+}
+
+export default teams
