f6a6134b81479439e700e3d6459fdefbeb0cfded
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-14 10:52:44 +0530

    style: edit navbar bg color

--- components/navbar/Navbar.styled.js
+++ components/navbar/Navbar.styled.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import Link from 'next/link';
+import styled from "styled-components";
+import Link from "next/link";
 
 export const MainContainer = styled.div`
   width: 100%;
@@ -8,20 +8,21 @@ export const MainContainer = styled.div`
   justify-content: center;
   align-items: center;
   overflow-x: hidden;
+  z-index: 10;
+  background-color: ${({ theme }) => theme.colors.bgPrimary};
 `;
+
 export const NavbarContainer = styled.nav`
   width: 90%;
   height: 80px;
   margin: 20px 20px;
-  background-color: #fff;
+  background-color: inherit;
   display: flex;
   flex-direction: column;
   -webkit-box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.25);
   -moz-box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.25);
   box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.25);
   border-radius: 18px;
-  top: 0px;
-  position: fixed;
 `;
 
 export const LeftContainer = styled.div`
@@ -30,6 +31,7 @@ export const LeftContainer = styled.div`
   justify-content: start;
   align-items: center;
   padding: 1rem 1rem;
+  background-color: inherit;
 `;
 
 export const RightContainer = styled.div`
@@ -37,12 +39,14 @@ export const RightContainer = styled.div`
   display: flex;
   justify-content: end;
   align-items: center;
+  background-color: inherit;
 `;
 
 export const NavbarInnerContainer = styled.div`
   width: 100%;
   height: 80px;
   display: flex;
+  background-color: inherit;
 `;
 
 export const NavbarLinkContainer = styled.div`
@@ -51,6 +55,7 @@ export const NavbarLinkContainer = styled.div`
   justify-content: end;
   margin-right: 2rem;
   align-items: center;
+  background-color: inherit;
 `;
 
 export const NavLink = styled(Link)`
