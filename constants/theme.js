6aea4fc942137a17f82e376d937fa31cd3401f69
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-15 15:28:14 +0530

    style: update breakpoints

--- constants/theme.js
+++ constants/theme.js
@@ -85,7 +85,7 @@ export const typography = {
   },
 };
 
-const breakpoints = {
+export const breakpoints = {
   xs: "320px",
   sm: "640px",
   md: "768px",
@@ -95,10 +95,10 @@ const breakpoints = {
 };
 
 export const devices = {
-  xs: `(min-width: ${breakpoints.xs})`,
-  sm: `(min-width: ${breakpoints.sm})`,
-  md: `(min-width: ${breakpoints.md})`,
-  lg: `(min-width: ${breakpoints.lg})`,
-  xl: `(min-width: ${breakpoints.xl})`,
-  "2xl": `(min-width: ${breakpoints["2xl"]})`,
+  xs: `(max-width: ${breakpoints.xs})`,
+  sm: `(max-width: ${breakpoints.sm})`,
+  md: `(max-width: ${breakpoints.md})`,
+  lg: `(max-width: ${breakpoints.lg})`,
+  xl: `(max-width: ${breakpoints.xl})`,
+  "2xl": `(max-width: ${breakpoints["2xl"]})`,
 };
