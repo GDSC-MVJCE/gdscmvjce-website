5fc3785f7ac41dd113c94804f1b55159578d7e72
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-17 23:23:17 +0530

    chore: update faq data

--- constants/faqData.js
+++ constants/faqData.js
@@ -1,99 +1,146 @@
-export const generalFAQ = [
-	{
-		question: "When is Google I/O 2023?",
-		answer: "This years event will be broadcast in front of a limited live audience and is open to everyone online on May 10, 2023. Tune in to the livestreamed keynotes, then dive into technical content and learning material on demand. Registration begins March 7 and is free of cost."
-	},
-	{
-		question:
-			"How will Google I/O 2023 work for attendees outside of the US?",
-		answer: "One of the exciting things about a digital experience is the opportunity to reach a global audience. Content will be provided on demand and in different languages to serve everyone. Some translated content such as captioned videos may be available shortly after the event. Check out what your local developer community is offering as well."
-	},
-	{
-		question: "How can I stay informed on the latest from Google I/O?",
-		answer: "Register to receive important information via email about the digital event. To stay up-to-date on the latest information on sessions, speakers, and other activities, check the Google I/O 2023 website, visit the Google Developers blog, and follow us on Twitter, LinkedIn, and Instagram. Join the social conversation about Google I/O 2023 via the official #GoogleIO hashtag."
-	},
-	{
-		question: "What will virtual content look like?",
-		answer: "Keynotes will be live and available on demand following the event. All sessions will be on demand to watch at your convenience. Check back on the site to see the full program."
-	}
-];
-
-export const registrationFAQ = [
-	{
-		question: "When is Google I/O 2023?",
-		answer: "This years event will be broadcast in front of a limited live audience and is open to everyone online on May 10, 2023. Tune in to the livestreamed keynotes, then dive into technical content and learning material on demand. Registration begins March 7 and is free of cost."
-	},
-	{
-		question:
-			"How will Google I/O 2023 work for attendees outside of the US?",
-		answer: "One of the exciting things about a digital experience is the opportunity to reach a global audience. Content will be provided on demand and in different languages to serve everyone. Some translated content such as captioned videos may be available shortly after the event. Check out what your local developer community is offering as well."
-	},
-	{
-		question: "How can I stay informed on the latest from Google I/O?",
-		answer: "Register to receive important information via email about the digital event. To stay up-to-date on the latest information on sessions, speakers, and other activities, check the Google I/O 2023 website, visit the Google Developers blog, and follow us on Twitter, LinkedIn, and Instagram. Join the social conversation about Google I/O 2023 via the official #GoogleIO hashtag."
-	},
-	{
-		question: "What will virtual content look like?",
-		answer: "Keynotes will be live and available on demand following the event. All sessions will be on demand to watch at your convenience. Check back on the site to see the full program."
-	}
-];
-
-export const applicationFAQ = [
-	{
-		question: "When is Google I/O 2023?",
-		answer: "This years event will be broadcast in front of a limited live audience and is open to everyone online on May 10, 2023. Tune in to the livestreamed keynotes, then dive into technical content and learning material on demand. Registration begins March 7 and is free of cost."
-	},
-	{
-		question:
-			"How will Google I/O 2023 work for attendees outside of the US?",
-		answer: "One of the exciting things about a digital experience is the opportunity to reach a global audience. Content will be provided on demand and in different languages to serve everyone. Some translated content such as captioned videos may be available shortly after the event. Check out what your local developer community is offering as well."
-	},
-	{
-		question: "How can I stay informed on the latest from Google I/O?",
-		answer: "Register to receive important information via email about the digital event. To stay up-to-date on the latest information on sessions, speakers, and other activities, check the Google I/O 2023 website, visit the Google Developers blog, and follow us on Twitter, LinkedIn, and Instagram. Join the social conversation about Google I/O 2023 via the official #GoogleIO hashtag."
-	},
-	{
-		question: "What will virtual content look like?",
-		answer: "Keynotes will be live and available on demand following the event. All sessions will be on demand to watch at your convenience. Check back on the site to see the full program."
-	}
-];
-
-export const communityFAQ = [
-	{
-		question: "When is Google I/O 2023?",
-		answer: "This years event will be broadcast in front of a limited live audience and is open to everyone online on May 10, 2023. Tune in to the livestreamed keynotes, then dive into technical content and learning material on demand. Registration begins March 7 and is free of cost."
-	},
-	{
-		question:
-			"How will Google I/O 2023 work for attendees outside of the US?",
-		answer: "One of the exciting things about a digital experience is the opportunity to reach a global audience. Content will be provided on demand and in different languages to serve everyone. Some translated content such as captioned videos may be available shortly after the event. Check out what your local developer community is offering as well."
-	},
-	{
-		question: "How can I stay informed on the latest from Google I/O?",
-		answer: "Register to receive important information via email about the digital event. To stay up-to-date on the latest information on sessions, speakers, and other activities, check the Google I/O 2023 website, visit the Google Developers blog, and follow us on Twitter, LinkedIn, and Instagram. Join the social conversation about Google I/O 2023 via the official #GoogleIO hashtag."
-	},
-	{
-		question: "What will virtual content look like?",
-		answer: "Keynotes will be live and available on demand following the event. All sessions will be on demand to watch at your convenience. Check back on the site to see the full program."
-	}
-];
-
-export const newsletterFAQ = [
-	{
-		question: "When is Google I/O 2023?",
-		answer: "This years event will be broadcast in front of a limited live audience and is open to everyone online on May 10, 2023. Tune in to the livestreamed keynotes, then dive into technical content and learning material on demand. Registration begins March 7 and is free of cost."
-	},
-	{
-		question:
-			"How will Google I/O 2023 work for attendees outside of the US?",
-		answer: "One of the exciting things about a digital experience is the opportunity to reach a global audience. Content will be provided on demand and in different languages to serve everyone. Some translated content such as captioned videos may be available shortly after the event. Check out what your local developer community is offering as well."
-	},
-	{
-		question: "How can I stay informed on the latest from Google I/O?",
-		answer: "Register to receive important information via email about the digital event. To stay up-to-date on the latest information on sessions, speakers, and other activities, check the Google I/O 2023 website, visit the Google Developers blog, and follow us on Twitter, LinkedIn, and Instagram. Join the social conversation about Google I/O 2023 via the official #GoogleIO hashtag."
-	},
-	{
-		question: "What will virtual content look like?",
-		answer: "Keynotes will be live and available on demand following the event. All sessions will be on demand to watch at your convenience. Check back on the site to see the full program."
-	}
-];
+export const faqData = {
+  general: [
+    {
+      question: "What is GDSC MVJCE?",
+      answer:
+        "GDSC MVJCE stands for Google Developer Student Clubs at MVJ College of Engineering. It is a student-led community supported by Google, aimed at fostering a vibrant learning environment for students interested in technology."
+    },
+    {
+      question: "What is the purpose of GDSC MVJCE?",
+      answer:
+        "GDSC MVJCE aims to bridge the gap between theoretical knowledge and real-world applications of technology by organizing workshops, events, and projects related to various Google technologies."
+    },
+    {
+      question: "Who can join GDSC MVJCE?",
+      answer:
+        " GDSC MVJCE is open to all students from MVJ College of Engineering who are passionate about technology, regardless of their field of study."
+    },
+    {
+      question: "Are there any membership fees for joining GDSC MVJCE?",
+      answer: "No, GDSC MVJCE membership is free of charge."
+    },
+    {
+      question: "How can I stay updated about GDSC MVJCE activities?",
+      answer:
+        "You can follow our official social media accounts, subscribe to our newsletter, and regularly visit our website for updates on upcoming events and activities."
+    },
+    {
+      question: "Is GDSC MVJCE only focused on Google technologies?",
+      answer:
+        "While GDSC MVJCE does emphasize Google technologies, we also cover a wide range of other technologies, programming languages, and tools in our events and workshops."
+    },
+    {
+      question: "Are non-technical students welcome at GDSC MVJCE events?",
+      answer:
+        "Absolutely! Our events are designed to cater to participants with varying levels of technical knowledge, and we encourage interdisciplinary learning."
+    }
+  ],
+  registration: [
+    {
+      question: "How do I RSVP for an event organized by GDSC MVJCE?",
+      answer:
+        "You can RSVP for an event through our website by navigating to the event page and clicking on the RSVP button."
+    },
+    {
+      question: "Is registration required to attend GDSC MVJCE events?",
+      answer:
+        "Yes, registration is typically required to help us manage the event logistics and ensure a better experience for all participants."
+    },
+    {
+      question:
+        "Do I need any prerequisites to register for GDSC MVJCE events?",
+      answer:
+        "Prerequisites, if any, will be mentioned on the event details page. Some events might require a basic understanding of certain technologies."
+    },
+    {
+      question:
+        "Can I bring a friend who is not from MVJ College to GDSC MVJCE events?",
+      answer:
+        "Most of our events are open to MVJ College students, but occasionally we might have events where external participants are also welcome. Check the event details for specific information."
+    }
+  ],
+  application: [
+    {
+      question: "How can I apply to join GDSC MVJCE?",
+      answer:
+        "We usually open applications each academic year. Keep an eye on our website and social media channels for application announcements."
+    },
+    {
+      question: "What are the selection criteria for joining GDSC MVJCE?",
+      answer:
+        "Selection criteria may vary depending on the domain chosen by the applicant, but typically we look for enthusiasm, willingness to learn, and a passion for technology. Specifics will be mentioned in the application announcement."
+    },
+    {
+      question: "Can freshmen (first-year students) apply to join GDSC MVJCE?",
+      answer:
+        "Yes, freshmen are welcome to apply. We value diversity and fresh perspectives within our community."
+    },
+    {
+      question: "What kind of commitment is expected from GDSC MVJCE members?",
+      answer:
+        "While the commitment level may vary based on your involvement, we encourage active participation in events, projects, and meetings to make the most of your membership."
+    },
+    {
+      question:
+        "Is there any coding or technical skill test as part of the application process?",
+      answer:
+        "Our application process focuses more on your enthusiasm, motivation, and potential to contribute to the community. We value diversity in skills and backgrounds."
+    }
+  ],
+  community: [
+    {
+      question: "What are the perks of being a part of GDSC MVJCE?",
+      answer:
+        "Being a member of GDSC MVJCE provides access to exclusive workshops, networking opportunities, projects, and the chance to collaborate with like-minded peers."
+    },
+    {
+      question: "Is GDSC MVJCE only for computer science students?",
+      answer:
+        "No, GDSC MVJCE is open to students from all disciplines who have an interest in technology."
+    },
+    {
+      question: "How can I contribute to the GDSC MVJCE community?",
+      answer:
+        "You can contribute by attending events, participating in projects, sharing your knowledge, and volunteering to help organize events."
+    },
+    {
+      question: "How can I collaborate with other members on projects?",
+      answer:
+        "We organize regular project brainstorming sessions and encourage members to participate. You can also connect with fellow members through our communication channels."
+    },
+    {
+      question: "What networking opportunities does GDSC MVJCE provide?",
+      answer:
+        "GDSC MVJCE offers opportunities to connect with industry professionals, alumni, and fellow students through workshops, hackathons, and tech talks."
+    }
+  ],
+  newsletter: [
+    {
+      question: "How often is the GDSC MVJCE newsletter published?",
+      answer:
+        "The newsletter is typically published on a monthly basis, highlighting recent events, upcoming activities, and featured articles."
+    },
+    {
+      question:
+        "What kind of content is included in the GDSC MVJCE newsletter?",
+      answer:
+        "The newsletter includes event recaps, member spotlights, articles on emerging technologies, and announcements related to the tech community."
+    },
+    {
+      question: "Can I suggest topics for the GDSC MVJCE newsletter?",
+      answer:
+        "Absolutely! We encourage members to suggest topics, share their insights, and contribute to the content of the newsletter. Reach out to us with your ideas."
+    },
+    {
+      question:
+        "Can I unsubscribe from the GDSC MVJCE newsletter if I no longer wish to receive it?",
+      answer:
+        "Yes, you can usually find an 'unsubscribe' link at the bottom of our newsletter emails. However, we recommend staying subscribed to stay updated on our activities."
+    },
+    {
+      question: "Can I contribute an article to the GDSC MVJCE newsletter?",
+      answer:
+        "Absolutely! We encourage members to contribute articles, tutorials, or insights related to technology. Reach out to our newsletter team to discuss your contribution."
+    }
+  ]
+};
