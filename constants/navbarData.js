a6df8be5791b4e525de79136260ba3e06594d4e3
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-19 00:44:19 +0530

    chore: add navbar data

---
+++ constants/navbarData.js
@@ -0,0 +1,26 @@
+export const navbarData = [
+  {
+    title: "Home",
+    path: "/"
+  },
+  {
+    title: "Events",
+    path: "/events"
+  },
+  {
+    title: "Team",
+    path: "/teams"
+  },
+  {
+    title: "Blogs",
+    path: "/blogs"
+  },
+  {
+    title: "FAQ",
+    path: "/faq"
+  },
+  {
+    title: "Registration",
+    path: "/registration"
+  }
+];
