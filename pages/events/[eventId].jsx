c89f3b0603ac6de59c218c64e3a3e0f80d68a827
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2023-08-18 23:42:51 +0530

    Add custom getHandlers for events and blogs.

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,11 +1,43 @@
 import EventPage from "@/components/eventPage/EventPage";
 import Navbar from "@/components/navbar/Navbar";
+import prisma from "@/lib/prisma";
 
 export default function Event() {
-	return (
-		<>
-			<Navbar />
-			<EventPage />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <EventPage />
+    </>
+  );
+}
+
+export const getStaticProps = async (ctx) => {
+  const { eventId } = ctx.params;
+  console.log(eventId);
+
+  return {
+    props: {
+      eventId
+    }
+  };
+};
+
+export async function getStaticPaths() {
+  const eventSlugs = await prisma.event.findMany({
+    select: {
+      slug: true
+    }
+  });
+  console.log("Loaded Slugs: ", eventSlugs);
+  const paths = eventSlugs.map((event) => {
+    return {
+      params: {
+        slug: event.slug
+      }
+    };
+  });
+  return {
+    paths: paths,
+    fallback: false // can also be true or 'blocking'
+  };
 }
