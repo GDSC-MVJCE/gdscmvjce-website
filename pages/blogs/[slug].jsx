5ae195e4b62b6a5c6a48f8f143dd85b18c1355bc
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2023-08-19 08:24:30 +0530

    Add ISR to blog and refactor blog

---
+++ pages/blogs/[slug].jsx
@@ -0,0 +1,71 @@
+import BlogArticle from "@/components/blog/BlogArticle";
+import Navbar from "@/components/navbar/Navbar";
+import prisma from "@/lib/prisma";
+import Head from "next/head";
+import React from "react";
+
+const BlogPage = ({ articleData }) => {
+  return (
+    <>
+      <Head>
+        <title>{articleData.title}</title>
+        <meta name="description" content={articleData.content} />
+        <meta property="og:title" content={articleData.title} />
+        <meta property="og:description" content={articleData.content} />
+        <meta property="og:image" content={articleData.thumbnail} />
+      </Head>
+      <Navbar />
+      <BlogArticle article={articleData} />
+    </>
+  );
+};
+
+export const getStaticProps = async (ctx) => {
+  const { slug } = ctx.params;
+  const blog = await prisma.blog.findUnique({
+    include: {
+      author: true,
+      tags: true
+    },
+    where: {
+      slug: slug
+    }
+  });
+  if (blog) {
+    blog.date = String(blog.date);
+    return {
+      props: {
+        articleData: blog
+      },
+      revalidate: 20
+    };
+  } else {
+    return {
+      notFound: true
+    };
+  }
+};
+
+export async function getStaticPaths() {
+  const blogs = await prisma.blog.findMany({
+    select: {
+      slug: true
+    }
+  });
+  if (blogs) {
+    const blogSlugs = blogs.filter((blog) => blog.slug !== null);
+    const paths = blogSlugs.map((blog) => {
+      return {
+        params: {
+          slug: blog.slug
+        }
+      };
+    });
+    return {
+      paths: paths,
+      fallback: false // can also be true or 'blocking'
+    };
+  }
+}
+
+export default BlogPage;
