24630b0797faba59b2282849224e7b68da70defc
Sumith <sumith2827@gmail.com>
2023-08-21 16:17:29 +0530

    fix: update schema

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,20 +1,11 @@
 const { PrismaClient } = require("@prisma/client");
 const { Speaker, Event } = require("./GDSC - Event List2");
+const { Profile } = require("./GDSC 2021-22");
 
 const prisma = new PrismaClient();
 
 const load = async () => {
   try {
-    // await prisma.event.deleteMany();
-    // await prisma.speaker.deleteMany();
-    // await prisma.event.createMany({
-    //   data: Event,
-    // });
-    // console.log("Events Created");
-    // await prisma.speaker.createMany({
-    //   data: Speaker,
-    // })
-    // console.log("Speakers Created");
     console.log("Seeding script");
   } catch (error) {
     console.error(error.message);
