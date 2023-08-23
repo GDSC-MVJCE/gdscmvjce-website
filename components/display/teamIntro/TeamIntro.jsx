a4b2d7cc67c7bfb87b0434213b07f4dc400f7e8c
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2023-08-23 19:23:51 +0530

    Fix TeamIntro responsiveness & add new components.

--- components/display/teamIntro/TeamIntro.jsx
+++ components/display/teamIntro/TeamIntro.jsx
@@ -1,63 +1,81 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import {
   TeamButton,
   TeamDescriptionSection,
   TeamIntroContainer,
   TeamIntroSectionWrapper,
-  TeamTitlesSection,
-} from './TeamIntro.styled';
-import Typography from '../typography/Typography';
-import Terminal from './subcomponents/terminal/Terminal';
-import MLIntro from './subcomponents/mlSection/MLIntro';
-import DesignSection from './subcomponents/designSection/DesignSection';
-import ContentSection from './subcomponents/contentSection/ContentSection';
-import CommunitySection from './subcomponents/communitySection/CommunitySection';
+  TeamTitlesSection
+} from "./TeamIntro.styled";
+import Typography from "../typography/Typography";
+import Terminal from "./subcomponents/terminal/Terminal";
+import MLIntro from "./subcomponents/mlSection/MLIntro";
+import DesignSection from "./subcomponents/designSection/DesignSection";
+import ContentSection from "./subcomponents/contentSection/ContentSection";
+import CommunitySection from "./subcomponents/communitySection/CommunitySection";
+import MobileDesignSection from "./subcomponents/designSection/mobileDesignSection/MobileDesignSection";
+import { devices } from "@/constants/theme";
 
 const TeamIntro = () => {
-  const [activeTeam, setActiveTeam] = useState('tech');
+  const [activeTeam, setActiveTeam] = useState("tech");
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(devices.lg);
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
   return (
     <TeamIntroContainer>
-      <Typography variant='h1'>Our dynamic team structure</Typography>
+      <Typography variant="h1">Our dynamic team structure</Typography>
       <TeamIntroSectionWrapper>
         <TeamTitlesSection>
           <TeamButton
-            onClick={() => setActiveTeam('tech')}
-            isSelected={activeTeam === 'tech'}
+            onClick={() => setActiveTeam("tech")}
+            isSelected={activeTeam === "tech"}
           >
             Technical
           </TeamButton>
           <TeamButton
-            onClick={() => setActiveTeam('android')}
-            isSelected={activeTeam === 'android'}
+            onClick={() => setActiveTeam("android")}
+            isSelected={activeTeam === "android"}
           >
-            ML & Android
+            {"ML & Android"}
           </TeamButton>
           <TeamButton
-            onClick={() => setActiveTeam('design')}
-            isSelected={activeTeam === 'design'}
+            onClick={() => setActiveTeam("design")}
+            isSelected={activeTeam === "design"}
           >
             Design
           </TeamButton>
           <TeamButton
-            onClick={() => setActiveTeam('content')}
-            isSelected={activeTeam === 'content'}
+            onClick={() => setActiveTeam("content")}
+            isSelected={activeTeam === "content"}
           >
             Content
           </TeamButton>
           <TeamButton
-            onClick={() => setActiveTeam('community')}
-            isSelected={activeTeam === 'community'}
+            onClick={() => setActiveTeam("community")}
+            isSelected={activeTeam === "community"}
           >
             Community Management
           </TeamButton>
         </TeamTitlesSection>
         <TeamDescriptionSection>
-          {activeTeam === 'tech' && <Terminal />}
-          {activeTeam === 'android' && <MLIntro />}
-          {activeTeam === 'design' && <DesignSection />}
-          {activeTeam === 'content' && <ContentSection />}
-          {activeTeam === 'community' && <CommunitySection />}
+          {activeTeam === "tech" && <Terminal />}
+          {activeTeam === "android" && <MLIntro />}
+          {activeTeam === "design" &&
+            (isMobile ? <MobileDesignSection /> : <DesignSection />)}
+          {activeTeam === "content" && <ContentSection />}
+          {activeTeam === "community" && <CommunitySection />}
         </TeamDescriptionSection>
       </TeamIntroSectionWrapper>
     </TeamIntroContainer>
