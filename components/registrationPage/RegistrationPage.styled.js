cff786cceec3dfe4fa9b30e0be0319f200563f7c
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-24 00:59:01 +0530

    style: style the buttons

--- components/registrationPage/RegistrationPage.styled.js
+++ components/registrationPage/RegistrationPage.styled.js
@@ -115,10 +115,58 @@ export const ApplyButton = styled.button`
   font-weight: 600;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+
+  &:active {
+    background-color: "red";
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%) scaleY(1) scaleX(1.25);
+    top: 100%;
+    width: 140%;
+    height: 180%;
+    background-color: rgba(0, 0, 0, 0.05);
+    border-radius: 50%;
+    transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+    z-index: -1;
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    left: 55%;
+    transform: translateX(-50%) scaleY(1) scaleX(1.45);
+    top: 180%;
+    width: 160%;
+    height: 190%;
+    background-color: ${({ theme }) => theme.colors.brandGreen};
+    border-radius: 50%;
+    transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+    z-index: -1;
+  }
 
   &:hover {
-    background-color: ${({ theme }) => theme.colors.brandBlue + 95};
+    color: #ffffff;
+    border: 1px solid ${({ theme }) => theme.colors.brandGreen};
     transform: scale(1.05);
+  }
+
+  &:hover:before {
+    top: -35%;
+    background-color: ${({ theme }) => theme.colors.brandGreen};
+    transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
+  }
+
+  &:hover:after {
+    top: -45%;
+    background-color: ${({ theme }) => theme.colors.brandGreen};
+    transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
   }
 
   @media screen and (${devices.md}) {
@@ -140,10 +188,58 @@ export const FAQButton = styled.button`
   font-weight: 600;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+
+  &:active {
+    background-color: "red";
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%) scaleY(1) scaleX(1.25);
+    top: 100%;
+    width: 140%;
+    height: 180%;
+    background-color: rgba(0, 0, 0, 0.05);
+    border-radius: 50%;
+    transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+    z-index: -1;
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    left: 55%;
+    transform: translateX(-50%) scaleY(1) scaleX(1.45);
+    top: 180%;
+    width: 160%;
+    height: 190%;
+    background-color: ${({ theme }) => theme.colors.brandRed};
+    border-radius: 50%;
+    transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+    z-index: -1;
+  }
 
   &:hover {
-    background-color: ${({ theme }) => theme.colors.bgSecondary};
+    color: #ffffff;
+    border: 1px solid ${({ theme }) => theme.colors.brandRed};
     transform: scale(1.05);
+  }
+
+  &:hover:before {
+    top: -35%;
+    background-color: ${({ theme }) => theme.colors.brandRed};
+    transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
+  }
+
+  &:hover:after {
+    top: -45%;
+    background-color: ${({ theme }) => theme.colors.brandRed};
+    transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
   }
 
   @media screen and (${devices.md}) {
