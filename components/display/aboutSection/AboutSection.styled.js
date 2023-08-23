cfef33fd389086de78e68257c6121e1766a6273d
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-23 22:27:01 +0530

    fix: changed svh to dvh

--- components/display/aboutSection/AboutSection.styled.js
+++ components/display/aboutSection/AboutSection.styled.js
@@ -6,7 +6,7 @@ export const AboutSectionWrapper = styled.section`
   width: 100%;
   height: 100%;
   min-height: 100vh;
-  min-height: 100svh;
+  min-height: 100dvh;
   display: flex;
   justify-content: center;
   align-items: center;
--- components/display/eventsSection/EventsSection.styled.js
+++ components/display/eventsSection/EventsSection.styled.js
@@ -4,7 +4,7 @@ import { styled } from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
-  min-height: 100svh;
+  min-height: 100dvh;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
