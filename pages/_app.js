7ffeeac106c7cc8318f19931c2c6cf603864c712
Shivam77607 <shivamsharma77607@gmail.com>
2023-08-25 00:01:55 +0530

    feat: add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import GlobalStyles from "@/constants/globalStyles";
 import AuthProvider from "@/components/AuthProvider";
 import GradientAnimation from "@/components/gradientAnimation/GradientAnimation";
 import Layout from "@/components/layout";
+import Head from "next/head";
 
 const myFont = localFont({
   src: [
@@ -29,16 +30,38 @@ const myFont = localFont({
 
 export default function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <ThemeProvider theme={lightTheme}>
-        <GlobalStyles />
-        <main className={myFont.className}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </main>
-        <GradientAnimation />
-      </ThemeProvider>
-    </AuthProvider>
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <AuthProvider>
+        <ThemeProvider theme={lightTheme}>
+          <GlobalStyles />
+          <main className={myFont.className}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </main>
+          <GradientAnimation />
+        </ThemeProvider>
+      </AuthProvider>
+    </>
   );
 }
