14d5c6281b19d91a73e8f8eaf373d4928ea54218
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2023-09-07 12:18:12 +0530

    Close Registration Application

--- components/registrationPage/RegistrationPage.jsx
+++ components/registrationPage/RegistrationPage.jsx
@@ -21,7 +21,7 @@ import ClipboardIcon from "@public/icons/clipboard.svg";
 import UserIcon from "@public/icons/user.svg";
 
 const applicationData = {
-  isOpen: true,
+  isOpen: false,
   link: "https://forms.gle/tFG8cDgtThaa3H4p8"
 };
 
