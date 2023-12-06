73aed7060f86fd80b225905ef3c7925018d66cb2
Sidharth 'R3FluX' Sreejil <94698713+sid-js@users.noreply.github.com>
2023-12-06 21:09:34 +0530

    Fix Rendering of Hero

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -142,210 +142,210 @@ const Hero = () => {
     };
   }, []);
 
-  return (
-    isClient && (
-      <HeroSectionContainer>
-        {!isMobile ? (
-          <ScrollContainer snap="none">
-            <ScrollPage>
-              <Animator animation={batch(Fade(0, 1), Sticky(), Zoom(8, 1))}>
-                <LogoContainer>
-                  <Image
-                    src="/logos/gdsc-logo.svg"
-                    alt="GDSC Logo"
-                    priority
-                    fill
+  return isClient ? (
+    <HeroSectionContainer>
+      {!isMobile ? (
+        <ScrollContainer snap="none">
+          <ScrollPage>
+            <Animator animation={batch(Fade(0, 1), Sticky(), Zoom(8, 1))}>
+              <LogoContainer>
+                <Image
+                  src="/logos/gdsc-logo.svg"
+                  alt="GDSC Logo"
+                  priority
+                  fill
+                />
+                <MouseContainer>
+                  <MouseScroll
+                    animate={{
+                      y: [0, 24, 0]
+                    }}
+                    transition={{
+                      duration: 1.5,
+                      repeat: Infinity,
+                      repeatType: "loop"
+                    }}
                   />
-                  <MouseContainer>
-                    <MouseScroll
-                      animate={{
-                        y: [0, 24, 0]
-                      }}
-                      transition={{
-                        duration: 1.5,
-                        repeat: Infinity,
-                        repeatType: "loop"
-                      }}
-                    />
-                  </MouseContainer>
-                </LogoContainer>
-              </Animator>
-            </ScrollPage>
-            <ScrollPage>
-              <Animator animation={batch(Fade(), Sticky(), MoveIn())}>
-                <HeroBackgroundContainer>
-                  <div className="mainText">
-                    <HeroTextSpan
-                      color="#EA4335"
-                      onMouseEnter={() => setIsTextHighlighted(true)}
-                      onMouseLeave={() => setIsTextHighlighted(false)}
+                </MouseContainer>
+              </LogoContainer>
+            </Animator>
+          </ScrollPage>
+          <ScrollPage>
+            <Animator animation={batch(Fade(), Sticky(), MoveIn())}>
+              <HeroBackgroundContainer>
+                <div className="mainText">
+                  <HeroTextSpan
+                    color="#EA4335"
+                    onMouseEnter={() => setIsTextHighlighted(true)}
+                    onMouseLeave={() => setIsTextHighlighted(false)}
+                  >
+                    Learn.
+                  </HeroTextSpan>
+                  <HeroTextSpan
+                    color="#4285F4"
+                    onMouseEnter={() => setIsAvatarHighlighted(true)}
+                    onMouseLeave={() => setIsAvatarHighlighted(false)}
+                  >
+                    Connect.
+                  </HeroTextSpan>
+                  <HeroTextSpan color="#0F9D58">Grow.</HeroTextSpan>
+                </div>
+                <Xwrapper>
+                  {heroTextElements.map((element, index) => (
+                    <HeroBackgroundTextSpan
+                      isHighlighted={isTextHighlighted}
+                      top={element.y}
+                      left={element.x}
+                      key={index}
+                      color={element.color}
+                      id={element.id}
+                      ref={refsById[element.id]}
+                      delay={element.delay}
                     >
-                      Learn.
-                    </HeroTextSpan>
-                    <HeroTextSpan
-                      color="#4285F4"
-                      onMouseEnter={() => setIsAvatarHighlighted(true)}
-                      onMouseLeave={() => setIsAvatarHighlighted(false)}
+                      {element.text}
+                    </HeroBackgroundTextSpan>
+                  ))}
+                  {heroAvatarElements.map((element, index) => (
+                    <HeroAvatarWrapper
+                      isHighlighted={isAvatarHighlighted}
+                      top={element.y}
+                      left={element.x}
+                      key={index}
+                      id={element.id}
+                      ref={refsById[element.id]}
+                      delay={element.delay}
                     >
-                      Connect.
-                    </HeroTextSpan>
-                    <HeroTextSpan color="#0F9D58">Grow.</HeroTextSpan>
-                  </div>
-                  <Xwrapper>
-                    {heroTextElements.map((element, index) => (
-                      <HeroBackgroundTextSpan
-                        isHighlighted={isTextHighlighted}
-                        top={element.y}
-                        left={element.x}
-                        key={index}
-                        color={element.color}
-                        id={element.id}
-                        ref={refsById[element.id]}
-                        delay={element.delay}
-                      >
-                        {element.text}
-                      </HeroBackgroundTextSpan>
-                    ))}
-                    {heroAvatarElements.map((element, index) => (
-                      <HeroAvatarWrapper
-                        isHighlighted={isAvatarHighlighted}
-                        top={element.y}
-                        left={element.x}
-                        key={index}
-                        id={element.id}
-                        ref={refsById[element.id]}
-                        delay={element.delay}
-                      >
-                        <Avatar
-                          size="lg"
-                          url={element.url}
-                          borderColor={element.borderColor}
-                          blur={true}
-                          priority={true}
-                        />
-                      </HeroAvatarWrapper>
-                    ))}
-                    <Xarrow
-                      start={refsById["a1"]}
-                      end={refsById["a2"]}
-                      showHead={false}
-                      showTail={false}
-                      startAnchor={"middle"}
-                      endAnchor={"middle"}
-                      headShape={"circle"}
-                      tailShape={"circle"}
-                      tailSize={3}
-                      headSize={3}
-                      curveness={0}
-                      strokeWidth={2}
-                      zIndex={-2}
-                      headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      lineColor={isAvatarHighlighted ? "#4285F4" : "#F1F1F1"}
-                    />
-                    <Xarrow
-                      start={refsById["a2"]}
-                      end={refsById["a4"]}
-                      showHead={false}
-                      showTail={false}
-                      startAnchor={"middle"}
-                      endAnchor={"middle"}
-                      headShape={"circle"}
-                      tailShape={"circle"}
-                      tailSize={3}
-                      headSize={3}
-                      curveness={0}
-                      strokeWidth={2}
-                      zIndex={-2}
-                      headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      lineColor={isAvatarHighlighted ? "#4285F4" : "#F1F1F1"}
-                    />
-                    <Xarrow
-                      start={refsById["a1"]}
-                      end={refsById["a3"]}
-                      showHead={false}
-                      showTail={false}
-                      startAnchor={"middle"}
-                      endAnchor={"middle"}
-                      headShape={"circle"}
-                      tailShape={"circle"}
-                      tailSize={3}
-                      headSize={3}
-                      curveness={0}
-                      strokeWidth={2}
-                      zIndex={-2}
-                      headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      lineColor={isAvatarHighlighted ? "#4285F4" : "#F1F1F1"}
-                    />
+                      <Avatar
+                        size="lg"
+                        url={element.url}
+                        borderColor={element.borderColor}
+                        blur={true}
+                        priority={true}
+                      />
+                    </HeroAvatarWrapper>
+                  ))}
+                  <Xarrow
+                    start={refsById["a1"]}
+                    end={refsById["a2"]}
+                    showHead={false}
+                    showTail={false}
+                    startAnchor={"middle"}
+                    endAnchor={"middle"}
+                    headShape={"circle"}
+                    tailShape={"circle"}
+                    tailSize={3}
+                    headSize={3}
+                    curveness={0}
+                    strokeWidth={2}
+                    zIndex={-2}
+                    headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    lineColor={isAvatarHighlighted ? "#4285F4" : "#F1F1F1"}
+                  />
+                  <Xarrow
+                    start={refsById["a2"]}
+                    end={refsById["a4"]}
+                    showHead={false}
+                    showTail={false}
+                    startAnchor={"middle"}
+                    endAnchor={"middle"}
+                    headShape={"circle"}
+                    tailShape={"circle"}
+                    tailSize={3}
+                    headSize={3}
+                    curveness={0}
+                    strokeWidth={2}
+                    zIndex={-2}
+                    headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    lineColor={isAvatarHighlighted ? "#4285F4" : "#F1F1F1"}
+                  />
+                  <Xarrow
+                    start={refsById["a1"]}
+                    end={refsById["a3"]}
+                    showHead={false}
+                    showTail={false}
+                    startAnchor={"middle"}
+                    endAnchor={"middle"}
+                    headShape={"circle"}
+                    tailShape={"circle"}
+                    tailSize={3}
+                    headSize={3}
+                    curveness={0}
+                    strokeWidth={2}
+                    zIndex={-2}
+                    headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    lineColor={isAvatarHighlighted ? "#4285F4" : "#F1F1F1"}
+                  />
 
-                    <Xarrow
-                      start="flutter"
-                      end="web"
-                      showHead={true}
-                      showTail={true}
-                      startAnchor={"top"}
-                      endAnchor={"right"}
-                      headShape={"circle"}
-                      tailShape={"circle"}
-                      tailSize={3}
-                      headSize={3}
-                      curveness={0}
-                      zIndex={-2}
-                      strokeWidth={2}
-                      headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      lineColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                    />
+                  <Xarrow
+                    start="flutter"
+                    end="web"
+                    showHead={true}
+                    showTail={true}
+                    startAnchor={"top"}
+                    endAnchor={"right"}
+                    headShape={"circle"}
+                    tailShape={"circle"}
+                    tailSize={3}
+                    headSize={3}
+                    curveness={0}
+                    zIndex={-2}
+                    strokeWidth={2}
+                    headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    lineColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                  />
 
-                    <Xarrow
-                      start="tensorflow"
-                      end="web"
-                      showHead={true}
-                      showTail={true}
-                      startAnchor={"right"}
-                      endAnchor={"left"}
-                      headShape={"circle"}
-                      tailShape={"circle"}
-                      tailSize={3}
-                      headSize={3}
-                      curveness={0}
-                      zIndex={-2}
-                      strokeWidth={2}
-                      headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      lineColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                    />
+                  <Xarrow
+                    start="tensorflow"
+                    end="web"
+                    showHead={true}
+                    showTail={true}
+                    startAnchor={"right"}
+                    endAnchor={"left"}
+                    headShape={"circle"}
+                    tailShape={"circle"}
+                    tailSize={3}
+                    headSize={3}
+                    curveness={0}
+                    zIndex={-2}
+                    strokeWidth={2}
+                    headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    lineColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                  />
 
-                    <Xarrow
-                      start="tensorflow"
-                      end="android"
-                      showHead={true}
-                      showTail={true}
-                      startAnchor={"left"}
-                      endAnchor={"bottom"}
-                      headShape={"circle"}
-                      tailShape={"circle"}
-                      tailSize={3}
-                      headSize={3}
-                      curveness={0}
-                      zIndex={-2}
-                      strokeWidth={2}
-                      headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                      lineColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
-                    />
-                  </Xwrapper>
-                </HeroBackgroundContainer>
-              </Animator>
-            </ScrollPage>
-          </ScrollContainer>
-        ) : (
-          <MobileHero />
-        )}
-      </HeroSectionContainer>
-    )
+                  <Xarrow
+                    start="tensorflow"
+                    end="android"
+                    showHead={true}
+                    showTail={true}
+                    startAnchor={"left"}
+                    endAnchor={"bottom"}
+                    headShape={"circle"}
+                    tailShape={"circle"}
+                    tailSize={3}
+                    headSize={3}
+                    curveness={0}
+                    zIndex={-2}
+                    strokeWidth={2}
+                    headColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    tailColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                    lineColor={isTextHighlighted ? "#EA4335" : "#F1F1F1"}
+                  />
+                </Xwrapper>
+              </HeroBackgroundContainer>
+            </Animator>
+          </ScrollPage>
+        </ScrollContainer>
+      ) : (
+        <MobileHero />
+      )}
+    </HeroSectionContainer>
+  ) : (
+    <></>
   );
 };
 
